2ecd7d2976e05cc46ce5ef55ad179ddf035604aa
brupes <brunniel.lopes@gmail.com>
2020-05-06 16:29:41 +0100

    Inicio do projeto

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String args[]){
+        print("teste");
+        print("teste");
+        print("teste");
+        print("teste");
+        print("teste");
+        print("teste");
+    }
+
+    public static void print(String text){
+        System.out.println(text);
+    }
+}
